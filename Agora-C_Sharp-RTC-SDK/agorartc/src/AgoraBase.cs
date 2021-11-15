eb3f65ab761ce2da845b0cd0a347509c8f36090d
mberky <polycer@163.com>
2021-11-15 14:08:00 +0800

    update readme; fix LogConfig invalid problem (#34)
    Co-authored-by: Jumin Wang <wangjumin@agora.io>

--- Agora-C_Sharp-RTC-SDK/agorartc/src/AgoraBase.cs
+++ Agora-C_Sharp-RTC-SDK/agorartc/src/AgoraBase.cs
@@ -5798,18 +5798,18 @@ Sets the sample rate, bitrate, encoding mode, and the number of channels:*/
          *
          * Ensure that the directory for the log files exists and is writable. You can use this parameter to rename the log files.
          */
-        string filePath { set; get; }
+        public string filePath { set; get; }
 
         /** The size (KB) of a log file. The default value is 1024 KB. If you set `fileSize` to 1024 KB, the SDK outputs at most 5 MB log files;
          * if you set it to less than 1024 KB, the setting is invalid, and the maximum size of a log file is still 1024 KB.
          */
-        int fileSize { set; get; }
+        public int fileSize { set; get; }
 
         /** The output log level of the SDK. See #LOG_LEVEL.
          *
          * For example, if you set the log level to WARN, the SDK outputs the logs within levels FATAL, ERROR, and WARN.
          */
-        LOG_LEVEL level { set; get; }
+        public LOG_LEVEL level { set; get; }
     };
 
     /** Configurations for the `IRtcEngine` instance.
--- CSharp-API_Example/src/Advanced/ScreenShare/ScreenShare.cs
+++ CSharp-API_Example/src/Advanced/ScreenShare/ScreenShare.cs
@@ -26,7 +26,7 @@ namespace CSharp_API_Example
         private string app_id_ = "";
         private string channel_id_ = "";
         private readonly string ScreenShare_TAG = "[ScreenShare] ";
-        private readonly string log_file_path = "CSharp_API_Example.log";
+        private readonly string agora_sdk_log_file_path_ = "agorasdk.log";
         private IAgoraRtcEngine rtc_engine_ = null;
         private IAgoraRtcEngineEventHandler event_handler_ = null;
         private IAgoraRtcEngine screen_share_engine_ = null;
@@ -50,12 +50,10 @@ namespace CSharp_API_Example
             {
                 rtc_engine_ = AgoraRtcEngine.CreateAgoraRtcEngine();
             }
-
-            RtcEngineContext rtc_engine_ctx = new RtcEngineContext(app_id_);
+            LogConfig log_config = new LogConfig(agora_sdk_log_file_path_);
+            RtcEngineContext rtc_engine_ctx = new RtcEngineContext(app_id_, AREA_CODE.AREA_CODE_GLOB, log_config);
             ret = rtc_engine_.Initialize(rtc_engine_ctx);
             CSharpForm.dump_handler_("Initialize", ret);
-            ret = rtc_engine_.SetLogFile(log_file_path);
-            CSharpForm.dump_handler_(ScreenShare_TAG + "SetLogFile", ret);
             event_handler_ = new ScreenShareEventHandler(this);
             rtc_engine_.InitEventHandler(event_handler_);
 
