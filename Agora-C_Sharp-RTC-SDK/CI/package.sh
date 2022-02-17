f6db96724b6a5f985660161dc0b6312bb45e4781
HugoChaan <47882165+HugoChaan@users.noreply.github.com>
2022-02-17 20:26:34 +0800

    MS-86694 refactor SDK 3.5.2 (#45)
    * MS-86694 refactor SDK 3.5.2
    
    * fix bug in engine dispose
    
    * fix bug in engine dispose
    
    * fix bug in engine dispose
    
    * fix bug in engine dispose
    
    * fix bug in engine dispose
    
    * fix bug in SetRemoteDefaultVideoStreamType
    
    * fix bug in CameraCapturerConfiguration
    
    Co-authored-by: CYG <cyg@CYGdeMBP-2.domain.name>
    Co-authored-by: CYG <cyg@CYGdeMacBook-Pro-2.local>

--- Agora-C_Sharp-RTC-SDK/CI/package.sh
+++ Agora-C_Sharp-RTC-SDK/CI/package.sh
@@ -116,7 +116,7 @@ cp $ANDROID_SRC_PATH/x86/Release/*.so "$ANDROID_DST_PATH"/libs/x86
 echo "[Unity CI] copying iOS ..."
 IOS_DST_PATH="$PLUGIN_PATH/Agora-Unity-RTC-SDK/Plugins/iOS"
 cp -PRf $IOS_SRC_PATH/RTC/Agora_*/libs/*.framework "$IOS_DST_PATH"
-cp -PRf $IOS_SRC_PATH/OS64COMBINED/Release/*.framework "$IOS_DST_PATH"
+cp -PRf $IOS_SRC_PATH/ALL_ARCHITECTURE/Release/*.framework "$IOS_DST_PATH"
 
 # macOS
 echo "[Unity CI] copying macOS ..."
