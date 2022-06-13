8faf43285e3eb596d08dad994000fbdda3bd5aed
夏羊群 <xiayangqun@agora.io>
2022-06-13 15:33:15 +0800

    Cherry-Pick fbfc411 from dev/3.8.201

--- Agora-C_Sharp-RTC-SDK/Code/Impl/Private/Impl/RtcEngineImpl.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Impl/Private/Impl/RtcEngineImpl.cs
@@ -548,7 +548,7 @@ namespace agora.rtc
 
         public string GetErrorDescription(int code)
         {
-            var param = new { };
+            var param = new { code };
             var json = AgoraJson.ToJson(param);
 
             var nRet = AgoraRtcNative.CallIrisApi(_irisRtcEngine, AgoraApiType.FUNC_RTCENGINE_GETERRORDESCRIPTION,
