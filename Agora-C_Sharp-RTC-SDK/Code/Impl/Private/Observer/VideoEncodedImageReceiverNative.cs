171f60ebc93f63aff8a6a237a72f2d6ab52a56f0
夏羊群 <xiayangqun@agora.io>
2022-09-14 20:54:40 +0800

    fix litjson for IntPtr

--- Agora-C_Sharp-RTC-SDK/Code/Impl/Private/Observer/VideoEncodedImageReceiverNative.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Impl/Private/Observer/VideoEncodedImageReceiverNative.cs
@@ -57,7 +57,7 @@ namespace Agora.Rtc
                     Marshal.Copy(length, lengthArray, 0, (int)buffer_count);
                 }
 
-              
+
 
                 switch (@event)
                 {
@@ -72,8 +72,8 @@ namespace Agora.Rtc
                             var p = new { result };
                             string json = AgoraJson.ToJson(p);
                             var jsonByte = System.Text.Encoding.Default.GetBytes(json);
-                             IntPtr resultPtr = (IntPtr)((UInt64)param + (UInt64)(IntPtr.Size * 2 + 4));
-                            Marshal.Copy(jsonByte, 0, resultPtr,(int)jsonByte.Length);
+                            IntPtr resultPtr = (IntPtr)((UInt64)param + (UInt64)(IntPtr.Size * 2 + 4));
+                            Marshal.Copy(jsonByte, 0, resultPtr, (int)jsonByte.Length);
                         }
                         break;
                     default:
@@ -96,8 +96,8 @@ namespace Agora.Rtc
                         var p = new { result };
                         string json = AgoraJson.ToJson(p);
                         var jsonByte = System.Text.Encoding.Default.GetBytes(json);
-                         IntPtr resultPtr = (IntPtr)((UInt64)param + (UInt64)(IntPtr.Size * 2 + 4));
-                            Marshal.Copy(jsonByte, 0, resultPtr,(int)jsonByte.Length);
+                        IntPtr resultPtr = (IntPtr)((UInt64)param + (UInt64)(IntPtr.Size * 2 + 4));
+                        Marshal.Copy(jsonByte, 0, resultPtr, (int)jsonByte.Length);
                     }
                     break;
                 default:
@@ -107,39 +107,39 @@ namespace Agora.Rtc
         }
 
 
-//#if UNITY_EDITOR_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_ANDROID 
-//        [MonoPInvokeCallback(typeof(Func_EncodedVideoFrameObserver_Native))]
-//#endif
-//        internal static bool OnEncodedVideoFrameReceived(uint uid, IntPtr imageBuffer, UInt64 length, IntPtr videoEncodedFrameInfoPtr)
-//        {
-//            if (videoEncodedFrameObserver == null)
-//                return true;
+        //#if UNITY_EDITOR_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_ANDROID 
+        //        [MonoPInvokeCallback(typeof(Func_EncodedVideoFrameObserver_Native))]
+        //#endif
+        //        internal static bool OnEncodedVideoFrameReceived(uint uid, IntPtr imageBuffer, UInt64 length, IntPtr videoEncodedFrameInfoPtr)
+        //        {
+        //            if (videoEncodedFrameObserver == null)
+        //                return true;
 
-//            var videoEncodedFrameInfo = (IrisEncodedVideoFrameInfo)(Marshal.PtrToStructure(videoEncodedFrameInfoPtr, typeof(IrisEncodedVideoFrameInfo)) ??
-//                new IrisEncodedVideoFrameInfo());
+        //            var videoEncodedFrameInfo = (IrisEncodedVideoFrameInfo)(Marshal.PtrToStructure(videoEncodedFrameInfoPtr, typeof(IrisEncodedVideoFrameInfo)) ??
+        //                new IrisEncodedVideoFrameInfo());
 
-//            var localVideoEncodedFrameInfo = LocalVideoEncodedVideoFrameInfo.info;
+        //            var localVideoEncodedFrameInfo = LocalVideoEncodedVideoFrameInfo.info;
 
-//            localVideoEncodedFrameInfo.codecType = (VIDEO_CODEC_TYPE)videoEncodedFrameInfo.codecType;
-//            localVideoEncodedFrameInfo.width = videoEncodedFrameInfo.width;
-//            localVideoEncodedFrameInfo.height = videoEncodedFrameInfo.height;
-//            localVideoEncodedFrameInfo.framesPerSecond = videoEncodedFrameInfo.framesPerSecond;
-//            localVideoEncodedFrameInfo.frameType = (VIDEO_FRAME_TYPE_NATIVE)videoEncodedFrameInfo.frameType;
-//            localVideoEncodedFrameInfo.rotation = (VIDEO_ORIENTATION)videoEncodedFrameInfo.rotation;
-//            localVideoEncodedFrameInfo.trackId = videoEncodedFrameInfo.trackId;
-//            localVideoEncodedFrameInfo.captureTimeMs = videoEncodedFrameInfo.captureTimeMs;
-//            localVideoEncodedFrameInfo.uid = videoEncodedFrameInfo.uid;
-//            localVideoEncodedFrameInfo.streamType = (VIDEO_STREAM_TYPE)videoEncodedFrameInfo.streamType;
+        //            localVideoEncodedFrameInfo.codecType = (VIDEO_CODEC_TYPE)videoEncodedFrameInfo.codecType;
+        //            localVideoEncodedFrameInfo.width = videoEncodedFrameInfo.width;
+        //            localVideoEncodedFrameInfo.height = videoEncodedFrameInfo.height;
+        //            localVideoEncodedFrameInfo.framesPerSecond = videoEncodedFrameInfo.framesPerSecond;
+        //            localVideoEncodedFrameInfo.frameType = (VIDEO_FRAME_TYPE_NATIVE)videoEncodedFrameInfo.frameType;
+        //            localVideoEncodedFrameInfo.rotation = (VIDEO_ORIENTATION)videoEncodedFrameInfo.rotation;
+        //            localVideoEncodedFrameInfo.trackId = videoEncodedFrameInfo.trackId;
+        //            localVideoEncodedFrameInfo.captureTimeMs = videoEncodedFrameInfo.captureTimeMs;
+        //            localVideoEncodedFrameInfo.uid = videoEncodedFrameInfo.uid;
+        //            localVideoEncodedFrameInfo.streamType = (VIDEO_STREAM_TYPE)videoEncodedFrameInfo.streamType;
 
-//            try
-//            {
-//                return videoEncodedFrameObserver.OnEncodedVideoFrameReceived(uid, imageBuffer, length, localVideoEncodedFrameInfo);
-//            }
-//            catch (Exception e)
-//            {
-//                AgoraLog.LogError("[Exception] IVideoEncodedFrameObserver.OnEncodedVideoFrameReceived: " + e);
-//                return false;
-//            }
-//        }
+        //            try
+        //            {
+        //                return videoEncodedFrameObserver.OnEncodedVideoFrameReceived(uid, imageBuffer, length, localVideoEncodedFrameInfo);
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                AgoraLog.LogError("[Exception] IVideoEncodedFrameObserver.OnEncodedVideoFrameReceived: " + e);
+        //                return false;
+        //            }
+        //        }
     }
 }
