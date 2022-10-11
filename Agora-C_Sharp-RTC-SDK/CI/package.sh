20d4288da53b786f517b1725729ff2794f166bbe
夏羊群 <xiayangqun@agora.io>
2022-10-11 11:19:17 +0800

    fix CI

--- Agora-C_Sharp-RTC-SDK/CI/package.sh
+++ Agora-C_Sharp-RTC-SDK/CI/package.sh
@@ -120,7 +120,7 @@ fi
 mkdir "$ANDROID_DST_PATH"/libs
 cp $ANDROID_SRC_PATH/DCG/Agora_*/rtc/sdk/*.jar "$ANDROID_DST_PATH"/libs
 
-if [ "$SDK_TYPE" == "video" ]; then
+if [ "$SDK_TYPE" == "video" && -f $ANDROID_SRC_PATH/DCG/Agora_*/rtc/sdk/*.aar ]; then
 cp $ANDROID_SRC_PATH/DCG/Agora_*/rtc/sdk/*.aar "$PLUGIN_PATH"/Agora-Unity-RTC-SDK/Plugins/Android
 fi
 
