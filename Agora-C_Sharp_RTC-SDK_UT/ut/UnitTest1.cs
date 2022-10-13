d09faeae1071df789bf13fc60aeabc8a7f4cef8c
夏羊群 <xiayangqun@agora.io>
2022-10-13 10:38:19 +0800

    add UT folder

---
+++ Agora-C_Sharp_RTC-SDK_UT/ut/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Agora.Rtc;
+
+namespace ut
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+           var hell = RtcEngine.CreateAgoraRtcEngine();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
