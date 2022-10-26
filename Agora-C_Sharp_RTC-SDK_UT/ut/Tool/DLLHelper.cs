b756866c5a158a20d774aa49b96295ba74ea63e2
夏羊群 <xiayangqun@agora.io>
2022-10-26 17:27:22 +0800

    add dellhelp

---
+++ Agora-C_Sharp_RTC-SDK_UT/ut/Tool/DLLHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ut
+{
+    public class DLLHelper
+    {
+        private const string  DebugLibName = "libName";
+
+        [DllImport(DebugLibName, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr CreateDebugApiEngine();
+    }
+}
