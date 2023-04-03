618adbbb6cfdf4748d3424a5c1bdd48894c7d69b
夏羊群 <xiayangqun@agora.io>
2023-04-03 17:49:45 +0800

    adapter with iris VIDEO_VIEW_SETUP_MODE

--- Agora-C_Sharp-RTC-SDK/Code/Rtc/VideoRender/VideoRender.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Rtc/VideoRender/VideoRender.cs
@@ -32,7 +32,7 @@ namespace Agora.Rtc
             _agoraRtcEngine = agoraRtcEngine;
             _agoraRtcEngine.OnRtcEngineImpleWillDispose += RtcEngineImplWillDispose;
             _videoFrameConfig = new IrisRtcVideoFrameConfig();
-            _videoFrameConfig.extra_identity = 0;
+            _videoFrameConfig.video_view_setup_mode = 0;
         }
 
         ~VideoStreamManager()
