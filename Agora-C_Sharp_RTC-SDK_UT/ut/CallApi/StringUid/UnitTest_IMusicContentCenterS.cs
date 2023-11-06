2abab2c61773dc7ce34a8ac5281353aa6b9648c9
夏羊群 <xiayangqun@agora.io>
2023-11-06 10:54:23 +0800

    save

---
+++ Agora-C_Sharp_RTC-SDK_UT/ut/CallApi/StringUid/UnitTest_IMusicContentCenterS.cs
@@ -0,0 +1,187 @@
+﻿using NUnit.Framework;
+using Agora.Rtc;
+namespace Agora.Rtc
+{
+    public class UnitTest_IMusicContentCenterS
+    {
+        public IRtcEngineS Engine;
+        public IMusicContentCenter MusicContentCenter;
+
+        [SetUp]
+        public void Setup()
+        {
+            Engine = RtcEngineS.CreateAgoraRtcEngine(DLLHelper.CreateFakeRtcEngineS());
+            RtcEngineContextS rtcEngineContext;
+            ParamsHelper.InitParam(out rtcEngineContext);
+            int nRet = Engine.Initialize(rtcEngineContext);
+            Assert.AreEqual(0, nRet);
+            MusicContentCenter = Engine.GetMusicContentCenter();
+        }
+
+        [TearDown]
+        public void TearDown() { Engine.Dispose(); }
+
+        #region terra IMusicContentCenter
+
+        [Test]
+        public void Test_Initialize()
+        {
+            MusicContentCenterConfiguration configuration = ParamsHelper.CreateParam<MusicContentCenterConfiguration>();
+
+            var nRet = MusicContentCenter.Initialize(configuration);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_RenewToken()
+        {
+            string token = ParamsHelper.CreateParam<string>();
+
+            var nRet = MusicContentCenter.RenewToken(token);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_RegisterEventHandler()
+        {
+            IMusicContentCenterEventHandler eventHandler = ParamsHelper.CreateParam<IMusicContentCenterEventHandler>();
+
+            var nRet = MusicContentCenter.RegisterEventHandler(eventHandler);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_UnregisterEventHandler()
+        {
+
+
+            var nRet = MusicContentCenter.UnregisterEventHandler();
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_CreateMusicPlayer()
+        {
+
+
+            var nRet = MusicContentCenter.CreateMusicPlayer();
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetMusicCharts()
+        {
+            string requestId = ParamsHelper.CreateParam<string>();
+
+            var nRet = MusicContentCenter.GetMusicCharts(ref requestId);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetMusicCollectionByMusicChartId()
+        {
+            string requestId = ParamsHelper.CreateParam<string>();
+            int musicChartId = ParamsHelper.CreateParam<int>();
+            int page = ParamsHelper.CreateParam<int>();
+            int pageSize = ParamsHelper.CreateParam<int>();
+            string jsonOption = ParamsHelper.CreateParam<string>();
+
+            var nRet = MusicContentCenter.GetMusicCollectionByMusicChartId(ref requestId, musicChartId, page, pageSize, jsonOption);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_SearchMusic()
+        {
+            string requestId = ParamsHelper.CreateParam<string>();
+            string keyWord = ParamsHelper.CreateParam<string>();
+            int page = ParamsHelper.CreateParam<int>();
+            int pageSize = ParamsHelper.CreateParam<int>();
+            string jsonOption = ParamsHelper.CreateParam<string>();
+
+            var nRet = MusicContentCenter.SearchMusic(ref requestId, keyWord, page, pageSize, jsonOption);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_Preload()
+        {
+            long songCode = ParamsHelper.CreateParam<long>();
+            string jsonOption = ParamsHelper.CreateParam<string>();
+
+            var nRet = MusicContentCenter.Preload(songCode, jsonOption);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_Preload2()
+        {
+            string requestId = ParamsHelper.CreateParam<string>();
+            long songCode = ParamsHelper.CreateParam<long>();
+
+            var nRet = MusicContentCenter.Preload(ref requestId, songCode);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_RemoveCache()
+        {
+            long songCode = ParamsHelper.CreateParam<long>();
+
+            var nRet = MusicContentCenter.RemoveCache(songCode);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetCaches()
+        {
+            MusicCacheInfo[] cacheInfo = ParamsHelper.CreateParam<MusicCacheInfo[]>();
+            int cacheInfoSize = ParamsHelper.CreateParam<int>();
+
+            var nRet = MusicContentCenter.GetCaches(ref cacheInfo, ref cacheInfoSize);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_IsPreloaded()
+        {
+            long songCode = ParamsHelper.CreateParam<long>();
+
+            var nRet = MusicContentCenter.IsPreloaded(songCode);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetLyric()
+        {
+            string requestId = ParamsHelper.CreateParam<string>();
+            long songCode = ParamsHelper.CreateParam<long>();
+            int LyricType = ParamsHelper.CreateParam<int>();
+
+            var nRet = MusicContentCenter.GetLyric(ref requestId, songCode, LyricType);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetSongSimpleInfo()
+        {
+            string requestId = ParamsHelper.CreateParam<string>();
+            long songCode = ParamsHelper.CreateParam<long>();
+
+            var nRet = MusicContentCenter.GetSongSimpleInfo(ref requestId, songCode);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetInternalSongCode()
+        {
+            long songCode = ParamsHelper.CreateParam<long>();
+            string jsonOption = ParamsHelper.CreateParam<string>();
+            long internalSongCode = ParamsHelper.CreateParam<long>();
+
+            var nRet = MusicContentCenter.GetInternalSongCode(songCode, jsonOption, ref internalSongCode);
+            Assert.AreEqual(0, nRet);
+        }
+        #endregion terra IMusicContentCenter
+    }
+}
---
+++ Agora-C_Sharp_RTC-SDK_UT/ut/CallApi/StringUid/UnitTest_IVideoDeviceManagerS.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using Agora.Rtc;
+using System;
+namespace Agora.Rtc
+{
+    public class UnitTest_IVideoDeviceManagerS
+    {
+        public IRtcEngineS Engine;
+        public IVideoDeviceManager VideoDeviceManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            Engine = RtcEngineS.CreateAgoraRtcEngine(DLLHelper.CreateFakeRtcEngineS());
+            RtcEngineContextS rtcEngineContext;
+            ParamsHelper.InitParam(out rtcEngineContext);
+            int nRet = Engine.Initialize(rtcEngineContext);
+            Assert.AreEqual(0, nRet);
+            VideoDeviceManager = Engine.GetVideoDeviceManager();
+            Assert.AreEqual(VideoDeviceManager != null, true);
+        }
+
+        [TearDown]
+        public void TearDown() { Engine.Dispose(); }
+
+        [Test]
+        public void Test_EnumerateVideoDevices()
+        {
+
+            var nRet = VideoDeviceManager.EnumerateVideoDevices();
+
+            Assert.AreEqual(0, nRet.Length);
+        }
+
+        #region terra IVideoDeviceManager
+
+        [Test]
+        public void Test_SetDevice()
+        {
+            string deviceIdUTF8 = ParamsHelper.CreateParam<string>();
+
+            var nRet = VideoDeviceManager.SetDevice(deviceIdUTF8);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetDevice()
+        {
+            string deviceIdUTF8 = ParamsHelper.CreateParam<string>();
+
+            var nRet = VideoDeviceManager.GetDevice(ref deviceIdUTF8);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_NumberOfCapabilities()
+        {
+            string deviceIdUTF8 = ParamsHelper.CreateParam<string>();
+
+            var nRet = VideoDeviceManager.NumberOfCapabilities(deviceIdUTF8);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_GetCapability()
+        {
+            string deviceIdUTF8 = ParamsHelper.CreateParam<string>();
+            uint deviceCapabilityNumber = ParamsHelper.CreateParam<uint>();
+            VideoFormat capability = ParamsHelper.CreateParam<VideoFormat>();
+
+            var nRet = VideoDeviceManager.GetCapability(deviceIdUTF8, deviceCapabilityNumber, ref capability);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_StartDeviceTest()
+        {
+            IntPtr hwnd = ParamsHelper.CreateParam<IntPtr>();
+
+            var nRet = VideoDeviceManager.StartDeviceTest(hwnd);
+            Assert.AreEqual(0, nRet);
+        }
+
+        [Test]
+        public void Test_StopDeviceTest()
+        {
+
+
+            var nRet = VideoDeviceManager.StopDeviceTest();
+            Assert.AreEqual(0, nRet);
+        }
+
+
+        #endregion terra IVideoDeviceManager
+    }
+}
