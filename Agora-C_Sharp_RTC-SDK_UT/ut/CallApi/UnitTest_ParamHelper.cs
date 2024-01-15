22751007e5bd02176af0ad60ad5ff6cd81c9e4af
xiayangqun <444746024@qq.com>
2024-01-15 14:42:34 +0800

    when salt lenght is less than 32, fill 0 with it (#356)
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>

--- Agora-C_Sharp_RTC-SDK_UT/ut/CallApi/UnitTest_ParamHelper.cs
+++ Agora-C_Sharp_RTC-SDK_UT/ut/CallApi/UnitTest_ParamHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using Agora.Rtc;
+using Agora.Rtm;
 
 namespace Agora.Rtc
 {
@@ -35,6 +36,60 @@ namespace Agora.Rtc
             context2.autoSubscribeVideo.SetEmpty();
             Assert.AreEqual(false, ParamsHelper.Compare<ChannelMediaOptions>(context1, context2));
 
+        }
+
+        [Test]
+        public void Test_RtmEncryptionConfig()
+        {
+            RtmEncryptionConfig rtmEncryptionConfig = new RtmEncryptionConfig();
+            var random = new Random();
+
+            //length is 32
+            var dest = new byte[32];
+            for (int i = 0; i < dest.Length; i++)
+            {
+                dest[i] = (byte)random.Next(0, 127);
+            }
+
+            rtmEncryptionConfig.encryptionSalt = dest;
+            var salt = rtmEncryptionConfig.encryptionSalt;
+            for (int i = 0; i < salt.Length; i++)
+            {
+                Assert.AreEqual(salt[i], dest[i]);
+            }
+
+            // length < 32
+            dest = new byte[random.Next(1, 31)];
+            for (int i = 0; i < dest.Length; i++)
+            {
+                dest[i] = (byte)random.Next(0, 127);
+            }
+            rtmEncryptionConfig.encryptionSalt = dest;
+            salt = rtmEncryptionConfig.encryptionSalt;
+            for (int i = 0; i < dest.Length; i++)
+            {
+                Assert.AreEqual(salt[i], dest[i]);
+            }
+
+            for (int i = dest.Length; i < 32; i++)
+            {
+                Assert.AreEqual(0, salt[i]);
+            }
+
+            // length > 33
+            dest = new byte[random.Next(33, 100)];
+            for (int i = 0; i < dest.Length; i++)
+            {
+                dest[i] = (byte)random.Next(0, 127);
+            }
+            try
+            {
+                rtmEncryptionConfig.encryptionSalt = dest;
+            }
+            catch (RTMException e)
+            {
+                Assert.AreEqual((int)RTM_ERROR_CODE.INVALID_ENCRYPTION_PARAMETER, e.Status.ErrorCode);
+            }
         }
     }
 }
