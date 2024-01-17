4e878981eade167c0ec7c9068b839374cc54dc23
夏羊群 <xiayangqun@agora.io>
2024-01-17 14:23:35 +0800

    call api id rebuild

--- Agora-C_Sharp-RTC-SDK/Code/Rtc/Impl/Private/Observer/H265TranscoderObserverNative.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Rtc/Impl/Private/Observer/H265TranscoderObserverNative.cs
@@ -45,7 +45,7 @@ namespace Agora.Rtc
             switch (@event)
             {
                 #region terra IH265TranscoderObserver
-                case "IH265TranscoderObserver__onEnableTranscode__agora_rtc_H265_TRANSCODE_RESULT":
+                case "H265TranscoderObserver_onEnableTranscode_6ba6646":
                     {
 #if UNITY_EDITOR_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_ANDROID
 CallbackObject._CallbackQueue.EnQueue(() => {
@@ -60,7 +60,7 @@ CallbackObject._CallbackQueue.EnQueue(() => {
                         break;
                     }
 
-                case "IH265TranscoderObserver__onQueryChannel__agora_rtc_H265_TRANSCODE_RESULT__const_char_ptr__const_char_ptr":
+                case "H265TranscoderObserver_onQueryChannel_31ba3df":
                     {
 #if UNITY_EDITOR_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_ANDROID
 CallbackObject._CallbackQueue.EnQueue(() => {
@@ -77,7 +77,7 @@ CallbackObject._CallbackQueue.EnQueue(() => {
                         break;
                     }
 
-                case "IH265TranscoderObserver__onTriggerTranscode__agora_rtc_H265_TRANSCODE_RESULT":
+                case "H265TranscoderObserver_onTriggerTranscode_6ba6646":
                     {
 #if UNITY_EDITOR_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_ANDROID
 CallbackObject._CallbackQueue.EnQueue(() => {
