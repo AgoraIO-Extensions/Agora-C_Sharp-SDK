996c1e7c990ee5bf9f74a28d0753140483dded5d
夏羊群 <xiayangqun@agora.io>
2024-07-09 12:02:09 +0800

    make ut pass

--- Agora-C_Sharp-RTC-SDK/Code/Rtm/Internal/Impl/Private/Native/AgoraApiType.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Rtm/Internal/Impl/Private/Native/AgoraApiType.cs
@@ -1,79 +1,115 @@
 ﻿namespace Agora.Rtm.Internal
 {
-    internal static class AgoraApiType
+    public class AgoraApiType
     {
-        #region IStreamChannel start
-        internal const string FUNC_STREAMCHANNEL_JOIN = "StreamChannel_join";
-        internal const string FUNC_STREAMCHANNEL_RENEWTOKEN = "StreamChannel_renewToken";
-        internal const string FUNC_STREAMCHANNEL_LEAVE = "StreamChannel_leave";
-        internal const string FUNC_STREAMCHANNEL_GETCHANNELNAME = "StreamChannel_getChannelName";
-        internal const string FUNC_STREAMCHANNEL_JOINTOPIC = "StreamChannel_joinTopic";
-        internal const string FUNC_STREAMCHANNEL_PUBLISHTOPICMESSAGE = "StreamChannel_publishTopicMessage";
-        internal const string FUNC_STREAMCHANNEL_LEAVETOPIC = "StreamChannel_leaveTopic";
-        internal const string FUNC_STREAMCHANNEL_SUBSCRIBETOPIC = "StreamChannel_subscribeTopic";
-        internal const string FUNC_STREAMCHANNEL_UNSUBSCRIBETOPIC = "StreamChannel_unsubscribeTopic";
-        internal const string FUNC_STREAMCHANNEL_GETSUBSCRIBEDUSERLIST = "StreamChannel_getSubscribedUserList";
-        internal const string FUNC_STREAMCHANNEL_RELEASE = "StreamChannel_release";
-        #endregion
 
-        #region RtmMetadata start
-        internal const string FUNC_METADATA_SETMAJORREVISION = "Metadata_setMajorRevision";
-        internal const string FUNC_METADATA_SETMETADATAITEM = "Metadata_setMetadataItem";
-        internal const string FUNC_METADATA_GETMETADATAITEMS = "Metadata_getMetadataItems";
-        internal const string FUNC_METADATA_CLEARMETADATA = "Metadata_clearMetadata";
-        internal const string FUNC_METADATA_RELEASE = "Metadata_release";
-        #endregion
+        /// IRtmEventHandler
+        public const string FUNC_RTMEVENTHANDLER_ONLINKSTATEEVENT = "RtmEventHandler_onLinkStateEvent_6d5317f";
+        public const string FUNC_RTMEVENTHANDLER_ONMESSAGEEVENT = "RtmEventHandler_onMessageEvent_ce5f8f5";
+        public const string FUNC_RTMEVENTHANDLER_ONPRESENCEEVENT = "RtmEventHandler_onPresenceEvent_32b5db1";
+        public const string FUNC_RTMEVENTHANDLER_ONTOPICEVENT = "RtmEventHandler_onTopicEvent_b4e4bd7";
+        public const string FUNC_RTMEVENTHANDLER_ONLOCKEVENT = "RtmEventHandler_onLockEvent_5e7fbef";
+        public const string FUNC_RTMEVENTHANDLER_ONSTORAGEEVENT = "RtmEventHandler_onStorageEvent_4304228";
+        public const string FUNC_RTMEVENTHANDLER_ONJOINRESULT = "RtmEventHandler_onJoinResult_ce14e01";
+        public const string FUNC_RTMEVENTHANDLER_ONLEAVERESULT = "RtmEventHandler_onLeaveResult_ce14e01";
+        public const string FUNC_RTMEVENTHANDLER_ONPUBLISHTOPICMESSAGERESULT = "RtmEventHandler_onPublishTopicMessageResult_ce14e01";
+        public const string FUNC_RTMEVENTHANDLER_ONJOINTOPICRESULT = "RtmEventHandler_onJoinTopicResult_bfd472e";
+        public const string FUNC_RTMEVENTHANDLER_ONLEAVETOPICRESULT = "RtmEventHandler_onLeaveTopicResult_bfd472e";
+        public const string FUNC_RTMEVENTHANDLER_ONSUBSCRIBETOPICRESULT = "RtmEventHandler_onSubscribeTopicResult_ae281ad";
+        public const string FUNC_RTMEVENTHANDLER_ONUNSUBSCRIBETOPICRESULT = "RtmEventHandler_onUnsubscribeTopicResult_ce14e01";
+        public const string FUNC_RTMEVENTHANDLER_ONGETSUBSCRIBEDUSERLISTRESULT = "RtmEventHandler_onGetSubscribedUserListResult_79f7ec8";
+        public const string FUNC_RTMEVENTHANDLER_ONCONNECTIONSTATECHANGED = "RtmEventHandler_onConnectionStateChanged_ac0fd53";
+        public const string FUNC_RTMEVENTHANDLER_ONTOKENPRIVILEGEWILLEXPIRE = "RtmEventHandler_onTokenPrivilegeWillExpire_3a2037f";
+        public const string FUNC_RTMEVENTHANDLER_ONSUBSCRIBERESULT = "RtmEventHandler_onSubscribeResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONUNSUBSCRIBERESULT = "RtmEventHandler_onUnsubscribeResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONPUBLISHRESULT = "RtmEventHandler_onPublishResult_4f46899";
+        public const string FUNC_RTMEVENTHANDLER_ONLOGINRESULT = "RtmEventHandler_onLoginResult_4f46899";
+        public const string FUNC_RTMEVENTHANDLER_ONLOGOUTRESULT = "RtmEventHandler_onLogoutResult_4f46899";
+        public const string FUNC_RTMEVENTHANDLER_ONRENEWTOKENRESULT = "RtmEventHandler_onRenewTokenResult_e9203be";
+        public const string FUNC_RTMEVENTHANDLER_ONSETCHANNELMETADATARESULT = "RtmEventHandler_onSetChannelMetadataResult_7cb1a6e";
+        public const string FUNC_RTMEVENTHANDLER_ONUPDATECHANNELMETADATARESULT = "RtmEventHandler_onUpdateChannelMetadataResult_7cb1a6e";
+        public const string FUNC_RTMEVENTHANDLER_ONREMOVECHANNELMETADATARESULT = "RtmEventHandler_onRemoveChannelMetadataResult_7cb1a6e";
+        public const string FUNC_RTMEVENTHANDLER_ONGETCHANNELMETADATARESULT = "RtmEventHandler_onGetChannelMetadataResult_d105bb5";
+        public const string FUNC_RTMEVENTHANDLER_ONSETUSERMETADATARESULT = "RtmEventHandler_onSetUserMetadataResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONUPDATEUSERMETADATARESULT = "RtmEventHandler_onUpdateUserMetadataResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONREMOVEUSERMETADATARESULT = "RtmEventHandler_onRemoveUserMetadataResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONGETUSERMETADATARESULT = "RtmEventHandler_onGetUserMetadataResult_7065706";
+        public const string FUNC_RTMEVENTHANDLER_ONSUBSCRIBEUSERMETADATARESULT = "RtmEventHandler_onSubscribeUserMetadataResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONUNSUBSCRIBEUSERMETADATARESULT = "RtmEventHandler_onUnsubscribeUserMetadataResult_edf84fc";
+        public const string FUNC_RTMEVENTHANDLER_ONSETLOCKRESULT = "RtmEventHandler_onSetLockResult_01034fa";
+        public const string FUNC_RTMEVENTHANDLER_ONREMOVELOCKRESULT = "RtmEventHandler_onRemoveLockResult_01034fa";
+        public const string FUNC_RTMEVENTHANDLER_ONRELEASELOCKRESULT = "RtmEventHandler_onReleaseLockResult_01034fa";
+        public const string FUNC_RTMEVENTHANDLER_ONACQUIRELOCKRESULT = "RtmEventHandler_onAcquireLockResult_aad7ec0";
+        public const string FUNC_RTMEVENTHANDLER_ONREVOKELOCKRESULT = "RtmEventHandler_onRevokeLockResult_01034fa";
+        public const string FUNC_RTMEVENTHANDLER_ONGETLOCKSRESULT = "RtmEventHandler_onGetLocksResult_6e98f5f";
+        public const string FUNC_RTMEVENTHANDLER_ONWHONOWRESULT = "RtmEventHandler_onWhoNowResult_b070777";
+        public const string FUNC_RTMEVENTHANDLER_ONGETONLINEUSERSRESULT = "RtmEventHandler_onGetOnlineUsersResult_b070777";
+        public const string FUNC_RTMEVENTHANDLER_ONWHERENOWRESULT = "RtmEventHandler_onWhereNowResult_ae64023";
+        public const string FUNC_RTMEVENTHANDLER_ONGETUSERCHANNELSRESULT = "RtmEventHandler_onGetUserChannelsResult_ae64023";
+        public const string FUNC_RTMEVENTHANDLER_ONPRESENCESETSTATERESULT = "RtmEventHandler_onPresenceSetStateResult_4f46899";
+        public const string FUNC_RTMEVENTHANDLER_ONPRESENCEREMOVESTATERESULT = "RtmEventHandler_onPresenceRemoveStateResult_4f46899";
+        public const string FUNC_RTMEVENTHANDLER_ONPRESENCEGETSTATERESULT = "RtmEventHandler_onPresenceGetStateResult_3d764cc";
+        /// IRtmEventHandler
+        /// IRtmClient
 
-        #region IRtmStorage start
-        internal const string FUNC_RTMSTORAGE_CREATEMETADATA = "RtmStorage_createMetadata";
-        internal const string FUNC_RTMSTORAGE_SETCHANNELMETADATA = "RtmStorage_setChannelMetadata";
-        internal const string FUNC_RTMSTORAGE_UPDATECHANNELMETADATA = "RtmStorage_updateChannelMetadata";
-        internal const string FUNC_RTMSTORAGE_REMOVECHANNELMETADATA = "RtmStorage_removeChannelMetadata";
-        internal const string FUNC_RTMSTORAGE_GETCHANNELMETADATA = "RtmStorage_getChannelMetadata";
-        internal const string FUNC_RTMSTORAGE_SETUSERMETADATA = "RtmStorage_setUserMetadata";
-        internal const string FUNC_RTMSTORAGE_UPDATEUSERMETADATA = "RtmStorage_updateUserMetadata";
-        internal const string FUNC_RTMSTORAGE_REMOVEUSERMETADATA = "RtmStorage_removeUserMetadata";
-        internal const string FUNC_RTMSTORAGE_GETUSERMETADATA = "RtmStorage_getUserMetadata";
-        internal const string FUNC_RTMSTORAGE_SUBSCRIBEUSERMETADATA = "RtmStorage_subscribeUserMetadata";
-        internal const string FUNC_RTMSTORAGE_UNSUBSCRIBEUSERMETADATA = "RtmStorage_unsubscribeUserMetadata";
-        #endregion
-
-        #region IRtmLock start
-        internal const string FUNC_RTMLOCK_SETLOCK = "RtmLock_setLock";
-        internal const string FUNC_RTMLOCK_GETLOCKS = "RtmLock_getLocks";
-        internal const string FUNC_RTMLOCK_REMOVELOCK = "RtmLock_removeLock";
-        internal const string FUNC_RTMLOCK_ACQUIRELOCK = "RtmLock_acquireLock";
-        internal const string FUNC_RTMLOCK_RELEASELOCK = "RtmLock_releaseLock";
-        internal const string FUNC_RTMLOCK_REVOKELOCK = "RtmLock_revokeLock";
-        #endregion
-
-        #region IRtmClient start
-        internal const string FUNC_RTMCLIENT_CREATE = "RtmClient_create";
-        internal const string FUNC_RTMCLIENT_RELEASE = "RtmClient_release";
-        internal const string FUNC_RTMCLIENT_LOGIN = "RtmClient_login";
-        internal const string FUNC_RTMCLIENT_LOGOUT = "RtmClient_logout";
-        internal const string FUNC_RTMCLIENT_GETSTORAGE = "RtmClient_getStorage";
-        internal const string FUNC_RTMCLIENT_GETLOCK = "RtmClient_getLock";
-        internal const string FUNC_RTMCLIENT_GETPRESENCE = "RtmClient_getPresence";
-        internal const string FUNC_RTMCLIENT_RENEWTOKEN = "RtmClient_renewToken";
-        internal const string FUNC_RTMCLIENT_PUBLISH = "RtmClient_publish";
-        internal const string FUNC_RTMCLIENT_SUBSCRIBE = "RtmClient_subscribe";
-        internal const string FUNC_RTMCLIENT_UNSUBSCRIBE = "RtmClient_unsubscribe";
-        internal const string FUNC_RTMCLIENT_CREATESTREAMCHANNEL = "RtmClient_createStreamChannel";
-        internal const string FUNC_RTMCLIENT_SETPARAMETERS = "RtmClient_setParameters";
-        internal const string FUNC_RTMCLIENT_SETLOGFILE = "RtmClient_setLogFile";
-        internal const string FUNC_RTMCLIENT_SETLOGLEVEL = "RtmClient_setLogLevel";
-        internal const string FUNC_RTMCLIENT_SETLOGFILESIZE = "RtmClient_setLogFileSize";
-        #endregion
-
-        #region IRtmPresence start
-        internal const string FUNC_RTMPRESENCE_WHONOW = "RtmPresence_whoNow";
-        internal const string FUNC_RTMPRESENCE_WHERENOW = "RtmPresence_whereNow";
-        internal const string FUNC_RTMPRESENCE_SETSTATE = "RtmPresence_setState";
-        internal const string FUNC_RTMPRESENCE_REMOVESTATE = "RtmPresence_removeState";
-        internal const string FUNC_RTMPRESENCE_GETSTATE = "RtmPresence_getState";
-        internal const string FUNC_RTMPRESENCE_GETONLINEUSERS = "RtmPresence_getOnlineUsers";
-        internal const string FUNC_RTMPRESENCE_GETUSERCHANNELS = "RtmPresence_getUserChannels";
-        #endregion
+        public const string FUNC_RTMCLIENT_CREATE = "RtmClient_create";
+        public const string FUNC_RTMCLIENT_SETLOGFILE = "RtmClient_setLogFile";
+        public const string FUNC_RTMCLIENT_SETLOGLEVEL = "RtmClient_setLogLevel";
+        public const string FUNC_RTMCLIENT_SETLOGFILESIZE = "RtmClient_setLogFileSize";
+        public const string FUNC_RTMCLIENT_SETAPPTYPE = "RtmClient_setAppType";
+        public const string FUNC_RTMCLIENT_RELEASE = "RtmClient_release";
+        public const string FUNC_RTMCLIENT_LOGIN = "RtmClient_login_1fa04dd";
+        public const string FUNC_RTMCLIENT_LOGOUT = "RtmClient_logout_90386a9";
+        public const string FUNC_RTMCLIENT_GETSTORAGE = "RtmClient_getStorage";
+        public const string FUNC_RTMCLIENT_GETLOCK = "RtmClient_getLock";
+        public const string FUNC_RTMCLIENT_GETPRESENCE = "RtmClient_getPresence";
+        public const string FUNC_RTMCLIENT_RENEWTOKEN = "RtmClient_renewToken_1fa04dd";
+        public const string FUNC_RTMCLIENT_PUBLISH = "RtmClient_publish_2d36e93";
+        public const string FUNC_RTMCLIENT_SUBSCRIBE = "RtmClient_subscribe_3fae92d";
+        public const string FUNC_RTMCLIENT_UNSUBSCRIBE = "RtmClient_unsubscribe_1fa04dd";
+        public const string FUNC_RTMCLIENT_CREATESTREAMCHANNEL = "RtmClient_createStreamChannel_ae3d0cf";
+        public const string FUNC_RTMCLIENT_SETPARAMETERS = "RtmClient_setParameters_3a2037f";
+        /// IRtmClient
+        /// IRtmLock
+        public const string FUNC_RTMLOCK_SETLOCK = "RtmLock_setLock_89e5672";
+        public const string FUNC_RTMLOCK_GETLOCKS = "RtmLock_getLocks_ad8568b";
+        public const string FUNC_RTMLOCK_REMOVELOCK = "RtmLock_removeLock_4ffa44d";
+        public const string FUNC_RTMLOCK_ACQUIRELOCK = "RtmLock_acquireLock_cd2dbc2";
+        public const string FUNC_RTMLOCK_RELEASELOCK = "RtmLock_releaseLock_4ffa44d";
+        public const string FUNC_RTMLOCK_REVOKELOCK = "RtmLock_revokeLock_fc4a9d7";
+        /// IRtmLock
+        /// IRtmPresence
+        public const string FUNC_RTMPRESENCE_WHONOW = "RtmPresence_whoNow_f7f61d1";
+        public const string FUNC_RTMPRESENCE_WHERENOW = "RtmPresence_whereNow_1fa04dd";
+        public const string FUNC_RTMPRESENCE_SETSTATE = "RtmPresence_setState_b73723a";
+        public const string FUNC_RTMPRESENCE_REMOVESTATE = "RtmPresence_removeState_d7033d8";
+        public const string FUNC_RTMPRESENCE_GETSTATE = "RtmPresence_getState_4ffa44d";
+        public const string FUNC_RTMPRESENCE_GETONLINEUSERS = "RtmPresence_getOnlineUsers_ce2d8e8";
+        public const string FUNC_RTMPRESENCE_GETUSERCHANNELS = "RtmPresence_getUserChannels_1fa04dd";
+        /// IRtmPresence
+        /// IRtmStorage
+        public const string FUNC_RTMSTORAGE_SETCHANNELMETADATA = "RtmStorage_setChannelMetadata_55e6d00";
+        public const string FUNC_RTMSTORAGE_UPDATECHANNELMETADATA = "RtmStorage_updateChannelMetadata_55e6d00";
+        public const string FUNC_RTMSTORAGE_REMOVECHANNELMETADATA = "RtmStorage_removeChannelMetadata_55e6d00";
+        public const string FUNC_RTMSTORAGE_GETCHANNELMETADATA = "RtmStorage_getChannelMetadata_ad8568b";
+        public const string FUNC_RTMSTORAGE_SETUSERMETADATA = "RtmStorage_setUserMetadata_24ae125";
+        public const string FUNC_RTMSTORAGE_UPDATEUSERMETADATA = "RtmStorage_updateUserMetadata_24ae125";
+        public const string FUNC_RTMSTORAGE_REMOVEUSERMETADATA = "RtmStorage_removeUserMetadata_24ae125";
+        public const string FUNC_RTMSTORAGE_GETUSERMETADATA = "RtmStorage_getUserMetadata_1fa04dd";
+        public const string FUNC_RTMSTORAGE_SUBSCRIBEUSERMETADATA = "RtmStorage_subscribeUserMetadata_1fa04dd";
+        public const string FUNC_RTMSTORAGE_UNSUBSCRIBEUSERMETADATA = "RtmStorage_unsubscribeUserMetadata_1fa04dd";
+        /// IRtmStorage
+        /// IStreamChannel
+        public const string FUNC_STREAMCHANNEL_JOIN = "StreamChannel_join_2090a6b";
+        public const string FUNC_STREAMCHANNEL_RENEWTOKEN = "StreamChannel_renewToken_1fa04dd";
+        public const string FUNC_STREAMCHANNEL_LEAVE = "StreamChannel_leave_90386a9";
+        public const string FUNC_STREAMCHANNEL_GETCHANNELNAME = "StreamChannel_getChannelName";
+        public const string FUNC_STREAMCHANNEL_JOINTOPIC = "StreamChannel_joinTopic_ff0ec3f";
+        public const string FUNC_STREAMCHANNEL_PUBLISHTOPICMESSAGE = "StreamChannel_publishTopicMessage_a31773e";
+        public const string FUNC_STREAMCHANNEL_LEAVETOPIC = "StreamChannel_leaveTopic_1fa04dd";
+        public const string FUNC_STREAMCHANNEL_SUBSCRIBETOPIC = "StreamChannel_subscribeTopic_b801234";
+        public const string FUNC_STREAMCHANNEL_UNSUBSCRIBETOPIC = "StreamChannel_unsubscribeTopic_b801234";
+        public const string FUNC_STREAMCHANNEL_GETSUBSCRIBEDUSERLIST = "StreamChannel_getSubscribedUserList_1fa04dd";
+        public const string FUNC_STREAMCHANNEL_RELEASE = "StreamChannel_release";
     }
 }
