4874586f23ac70b3accd9a8b8d9f2c124a602636
夏羊群 <xiayangqun@agora.io>
2025-03-26 15:05:58 +0800

    api type

--- Agora-C_Sharp-RTC-SDK/Code/Rtc/Impl/Private/Native/AgoraApiType.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Rtc/Impl/Private/Native/AgoraApiType.cs
@@ -4,27 +4,27 @@ using System;
 
 namespace Agora.Rtc
 {
-    internal partial class AgoraApiType
+    public partial class AgoraApiType
     {
-        internal const string IRTCENGINE_UNREGISTERAUDIOENCODEDFRAMEOBSERVER = "RtcEngine_unregisterAudioEncodedFrameObserver";
-        internal const string IRTCENGINE_SETAPPTYPE = "RtcEngine_setAppType";
-        internal const string IRTCENGINE_RELEASESCREENCAPTURESOURCES = "RtcEngine_releaseScreenCaptureSources";
-        internal const string IRTCENGINE_GETNATIVEHANDLE = "RtcEngine_getNativeHandle";
-        internal const string IRTCENGINE_SETMAXMETADATASIZE = "RtcEngine_setMaxMetadataSize";
-        internal const string IRTCENGINE_SENDMETADATA = "RtcEngine_sendMetaData";
-        internal const string IRTCENGINE_RELEASE = "RtcEngine_release";
+        public const string IRTCENGINE_UNREGISTERAUDIOENCODEDFRAMEOBSERVER = "RtcEngine_unregisterAudioEncodedFrameObserver";
+        public const string IRTCENGINE_SETAPPTYPE = "RtcEngine_setAppType";
+        public const string IRTCENGINE_RELEASESCREENCAPTURESOURCES = "RtcEngine_releaseScreenCaptureSources";
+        public const string IRTCENGINE_GETNATIVEHANDLE = "RtcEngine_getNativeHandle";
+        public const string IRTCENGINE_SETMAXMETADATASIZE = "RtcEngine_setMaxMetadataSize";
+        public const string IRTCENGINE_SENDMETADATA = "RtcEngine_sendMetaData";
+        public const string IRTCENGINE_RELEASE = "RtcEngine_release";
 
-        internal const string IMEDIAPLAYER_UNOPENWITHMEDIASOURCE = "MediaPlayer_unOpenWithMediaSource";
+        public const string IMEDIAPLAYER_UNOPENWITHMEDIASOURCE = "MediaPlayer_unOpenWithMediaSource";
 
-        internal const string IAUDIODEVICEMANAGER_GETPLAYBACKDEFAULTDEVICE = "AudioDeviceManager_getPlaybackDefaultDevice";
-        internal const string IAUDIODEVICEMANAGER_GETRECORDINGDEAFULTDEVICE = "AudioDeviceManager_getRecordingDefaultDevice";
+        public const string IAUDIODEVICEMANAGER_GETPLAYBACKDEFAULTDEVICE = "AudioDeviceManager_getPlaybackDefaultDevice";
+        public const string IAUDIODEVICEMANAGER_GETRECORDINGDEAFULTDEVICE = "AudioDeviceManager_getRecordingDefaultDevice";
 
-        internal const string IMEDIAENGINE_UNREGISTERAUDIOFRAMEOBSERVER = "MediaEngine_unregisterAudioFrameObserver";
-        internal const string IMEDIAENGINE_UNREGISTERVIDEOFRAMEOBSERVER = "MediaEngine_unregisterVideoFrameObserver";
-        internal const string IMEDIAENGINE_UNREGISTERVIDEOENCODEDFRAMEOBSERVER = "MediaEngine_unregisterVideoEncodedFrameObserver";
-        internal const string IMEDIAENGINE_UNREGISTERFACEINFOOBSERVER = "MediaEngine_unregisterFaceInfoObserver";
+        public const string IMEDIAENGINE_UNREGISTERAUDIOFRAMEOBSERVER = "MediaEngine_unregisterAudioFrameObserver";
+        public const string IMEDIAENGINE_UNREGISTERVIDEOFRAMEOBSERVER = "MediaEngine_unregisterVideoFrameObserver";
+        public const string IMEDIAENGINE_UNREGISTERVIDEOENCODEDFRAMEOBSERVER = "MediaEngine_unregisterVideoEncodedFrameObserver";
+        public const string IMEDIAENGINE_UNREGISTERFACEINFOOBSERVER = "MediaEngine_unregisterFaceInfoObserver";
 
-        internal const string IMEDIARECORDER_UNSETMEDIARECORDEROBSERVER = "MediaRecorder_unsetMediaRecorderObserver";
+        public const string IMEDIARECORDER_UNSETMEDIARECORDEROBSERVER = "MediaRecorder_unsetMediaRecorderObserver";
 
     }
 }
