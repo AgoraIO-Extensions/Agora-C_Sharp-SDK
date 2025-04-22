8a2f4afc7653c3e9a6a825719f9e60f8cefb14d1
夏羊群 <xiayangqun@agora.io>
2025-04-22 11:53:16 +0800

    remove commond

--- Agora-C_Sharp-RTC-SDK/Code/Rtc/VideoRender/VideoSurfaceYUV.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Rtc/VideoRender/VideoSurfaceYUV.cs
@@ -13,182 +13,182 @@ namespace Agora.Rtc
     ///
     public class VideoSurfaceYUV : VideoSurface
     {
-        //private TextureManagerYUV _textureManagerYUV;
-        //protected Material _material = null;
-        //protected float YStrideScale = 1.0f;
+        private TextureManagerYUV _textureManagerYUV;
+        protected Material _material = null;
+        protected float YStrideScale = 1.0f;
 
-        //void Start()
-        //{
-        //    FrameType = VIDEO_OBSERVER_FRAME_TYPE.FRAME_TYPE_YUV420;
-        //    CheckVideoSurfaceType();
-        //}
+        void Start()
+        {
+            FrameType = VIDEO_OBSERVER_FRAME_TYPE.FRAME_TYPE_YUV420;
+            CheckVideoSurfaceType();
+        }
 
-        //void Update()
-        //{
-        //    if (_renderer == null || _needUpdateInfo) return;
+        void Update()
+        {
+            if (_renderer == null || _needUpdateInfo) return;
 
-        //    if (Enable)
-        //    {
-        //        if (_textureManagerYUV == null)
-        //        {
-        //            string textureManagerName = this.GenerateTextureManagerUniqueName();
-        //            _TextureManagerGameObject = GameObject.Find(textureManagerName);
+            if (Enable)
+            {
+                if (_textureManagerYUV == null)
+                {
+                    string textureManagerName = this.GenerateTextureManagerUniqueName();
+                    _TextureManagerGameObject = GameObject.Find(textureManagerName);
 
-        //            if (_TextureManagerGameObject == null)
-        //            {
-        //                _TextureManagerGameObject = new GameObject(textureManagerName);
-        //                _TextureManagerGameObject.hideFlags = HideFlags.HideInHierarchy;
+                    if (_TextureManagerGameObject == null)
+                    {
+                        _TextureManagerGameObject = new GameObject(textureManagerName);
+                        _TextureManagerGameObject.hideFlags = HideFlags.HideInHierarchy;
 
-        //                _textureManagerYUV = _TextureManagerGameObject.AddComponent<TextureManagerYUV>();
-        //                _textureManagerYUV.SetVideoStreamIdentity(Uid, ChannelId, SourceType, FrameType);
-        //                _textureManagerYUV.EnableVideoFrameWithIdentity();
-        //            }
-        //            else
-        //            {
-        //                _textureManagerYUV = _TextureManagerGameObject.GetComponent<TextureManagerYUV>();
-        //            }
-        //        }
-        //        else if (_textureManagerYUV && !_hasAttach && _textureManagerYUV.CanTextureAttach())
-        //        {
-        //            UpdateShader();
-        //            ApplyTexture(_textureManagerYUV.YTexture, _textureManagerYUV.UTexture, _textureManagerYUV.VTexture);
-        //            _textureManagerYUV.Attach();
-        //            _hasAttach = true;
-        //        }
+                        _textureManagerYUV = _TextureManagerGameObject.AddComponent<TextureManagerYUV>();
+                        _textureManagerYUV.SetVideoStreamIdentity(Uid, ChannelId, SourceType, FrameType);
+                        _textureManagerYUV.EnableVideoFrameWithIdentity();
+                    }
+                    else
+                    {
+                        _textureManagerYUV = _TextureManagerGameObject.GetComponent<TextureManagerYUV>();
+                    }
+                }
+                else if (_textureManagerYUV && !_hasAttach && _textureManagerYUV.CanTextureAttach())
+                {
+                    UpdateShader();
+                    ApplyTexture(_textureManagerYUV.YTexture, _textureManagerYUV.UTexture, _textureManagerYUV.VTexture);
+                    _textureManagerYUV.Attach();
+                    _hasAttach = true;
+                }
 
-        //        if (_textureManagerYUV)
-        //        {
-        //            if (this._textureWidth != _textureManagerYUV.Width || this._textureHeight != _textureManagerYUV.Height)
-        //            {
-        //                this._textureWidth = _textureManagerYUV.Width;
-        //                this._textureHeight = _textureManagerYUV.Height;
+                if (_textureManagerYUV)
+                {
+                    if (this._textureWidth != _textureManagerYUV.Width || this._textureHeight != _textureManagerYUV.Height)
+                    {
+                        this._textureWidth = _textureManagerYUV.Width;
+                        this._textureHeight = _textureManagerYUV.Height;
 
-        //                if (this._textureWidth != 0 && this._textureHeight != 0)
-        //                {
-        //                    this.InvokeOnTextureSizeModify();
-        //                }
-        //            }
+                        if (this._textureWidth != 0 && this._textureHeight != 0)
+                        {
+                            this.InvokeOnTextureSizeModify();
+                        }
+                    }
 
-        //            if (this._textureWidth != 0 && this._textureHeight != 0 && this.YStrideScale != this._textureManagerYUV.YStrideScale)
-        //            {
-        //                if (_material != null)
-        //                {
-        //                    _material.SetFloat("_yStrideScale", _textureManagerYUV.YStrideScale);
-        //                }
-        //                this.YStrideScale = this._textureManagerYUV.YStrideScale;
-        //            }
+                    if (this._textureWidth != 0 && this._textureHeight != 0 && this.YStrideScale != this._textureManagerYUV.YStrideScale)
+                    {
+                        if (_material != null)
+                        {
+                            _material.SetFloat("_yStrideScale", _textureManagerYUV.YStrideScale);
+                        }
+                        this.YStrideScale = this._textureManagerYUV.YStrideScale;
+                    }
 
-        //        }
-        //    }
-        //    else
-        //    {
-        //        if (_hasAttach && !IsBlankTexture())
-        //        {
-        //            DestroyTextureManager();
-        //            ApplyTexture(null, null, null);
-        //        }
-        //    }
-        //}
+                }
+            }
+            else
+            {
+                if (_hasAttach && !IsBlankTexture())
+                {
+                    DestroyTextureManager();
+                    ApplyTexture(null, null, null);
+                }
+            }
+        }
 
-        //void OnDestroy()
-        //{
-        //    AgoraLog.Log(string.Format("VideoSurface YUV channel: ${0}, user:{1} destroy", ChannelId, Uid));
-        //    DestroyTextureManager();
-        //}
+        void OnDestroy()
+        {
+            AgoraLog.Log(string.Format("VideoSurface YUV channel: ${0}, user:{1} destroy", ChannelId, Uid));
+            DestroyTextureManager();
+        }
 
-        //protected override void CheckVideoSurfaceType()
-        //{
-        //    if (VideoSurfaceType == VideoSurfaceType.Renderer)
-        //    {
-        //        _renderer = GetComponent<Renderer>();
-        //    }
+        protected override void CheckVideoSurfaceType()
+        {
+            if (VideoSurfaceType == VideoSurfaceType.Renderer)
+            {
+                _renderer = GetComponent<Renderer>();
+            }
 
-        //    if (_renderer == null || VideoSurfaceType == VideoSurfaceType.RawImage)
-        //    {
-        //        _renderer = GetComponent<RawImage>();
-        //        if (_renderer != null)
-        //        {
-        //            VideoSurfaceType = VideoSurfaceType.RawImage;
-        //        }
-        //    }
+            if (_renderer == null || VideoSurfaceType == VideoSurfaceType.RawImage)
+            {
+                _renderer = GetComponent<RawImage>();
+                if (_renderer != null)
+                {
+                    VideoSurfaceType = VideoSurfaceType.RawImage;
+                }
+            }
 
-        //    if (_renderer == null)
-        //    {
-        //        AgoraLog.LogError("Unable to find surface render in VideoSurfaceYUV component.");
-        //    }
-        //}
+            if (_renderer == null)
+            {
+                AgoraLog.LogError("Unable to find surface render in VideoSurfaceYUV component.");
+            }
+        }
 
-        //protected override bool IsBlankTexture()
-        //{
-        //    if (VideoSurfaceType == VideoSurfaceType.Renderer)
-        //    {
-        //        var rd = (_renderer as Renderer);
-        //        return rd.material.mainTexture == null || !(rd.material.mainTexture is Texture2D);
-        //    }
-        //    else if (VideoSurfaceType == VideoSurfaceType.RawImage)
-        //    {
-        //        var rd = (_renderer as RawImage);
-        //        return (rd.texture == null);
-        //    }
-        //    else
-        //    {
-        //        return true;
-        //    }
-        //}
+        protected override bool IsBlankTexture()
+        {
+            if (VideoSurfaceType == VideoSurfaceType.Renderer)
+            {
+                var rd = (_renderer as Renderer);
+                return rd.material.mainTexture == null || !(rd.material.mainTexture is Texture2D);
+            }
+            else if (VideoSurfaceType == VideoSurfaceType.RawImage)
+            {
+                var rd = (_renderer as RawImage);
+                return (rd.texture == null);
+            }
+            else
+            {
+                return true;
+            }
+        }
 
-        //protected void ApplyTexture(Texture2D texture, Texture2D uTexture, Texture2D vTexture)
-        //{
-        //    if (VideoSurfaceType == VideoSurfaceType.Renderer)
-        //    {
-        //        var rd = _renderer as Renderer;
-        //        rd.material.mainTexture = texture;
-        //        rd.material.SetTexture("_UTex", uTexture);
-        //        rd.material.SetTexture("_VTex", vTexture);
-        //    }
-        //    else if (VideoSurfaceType == VideoSurfaceType.RawImage)
-        //    {
-        //        var rd = _renderer as RawImage;
-        //        rd.texture = texture;
-        //        rd.material.SetTexture("_UTex", uTexture);
-        //        rd.material.SetTexture("_VTex", vTexture);
-        //    }
-        //}
+        protected void ApplyTexture(Texture2D texture, Texture2D uTexture, Texture2D vTexture)
+        {
+            if (VideoSurfaceType == VideoSurfaceType.Renderer)
+            {
+                var rd = _renderer as Renderer;
+                rd.material.mainTexture = texture;
+                rd.material.SetTexture("_UTex", uTexture);
+                rd.material.SetTexture("_VTex", vTexture);
+            }
+            else if (VideoSurfaceType == VideoSurfaceType.RawImage)
+            {
+                var rd = _renderer as RawImage;
+                rd.texture = texture;
+                rd.material.SetTexture("_UTex", uTexture);
+                rd.material.SetTexture("_VTex", vTexture);
+            }
+        }
 
-        //protected override void DestroyTextureManager()
-        //{
-        //    if (_textureManagerYUV == null) return;
+        protected override void DestroyTextureManager()
+        {
+            if (_textureManagerYUV == null) return;
 
-        //    if (_hasAttach == true)
-        //    {
-        //        _textureManagerYUV.Detach();
-        //        _hasAttach = false;
-        //    }
+            if (_hasAttach == true)
+            {
+                _textureManagerYUV.Detach();
+                _hasAttach = false;
+            }
 
-        //    if (_textureManagerYUV.GetRefCount() <= 0)
-        //    {
-        //        Destroy(_TextureManagerGameObject);
-        //    }
-        //    _textureManagerYUV = null;
-        //}
+            if (_textureManagerYUV.GetRefCount() <= 0)
+            {
+                Destroy(_TextureManagerGameObject);
+            }
+            _textureManagerYUV = null;
+        }
 
-        //protected override void UpdateShader()
-        //{
+        protected override void UpdateShader()
+        {
 
-        //    if (VideoSurfaceType == VideoSurfaceType.Renderer)
-        //    {
-        //        var rd = _renderer as Renderer;
-        //        rd.material = new Material(Shader.Find("Unlit/RendererShader601"));
-        //        _material = rd.material;
-        //    }
-        //    else if (VideoSurfaceType == VideoSurfaceType.RawImage)
-        //    {
-        //        var rd = _renderer as RawImage;
-        //        rd.material = new Material(Shader.Find("UI/RendererShader601"));
-        //        _material = rd.material;
-        //    }
+            if (VideoSurfaceType == VideoSurfaceType.Renderer)
+            {
+                var rd = _renderer as Renderer;
+                rd.material = new Material(Shader.Find("Unlit/RendererShader601"));
+                _material = rd.material;
+            }
+            else if (VideoSurfaceType == VideoSurfaceType.RawImage)
+            {
+                var rd = _renderer as RawImage;
+                rd.material = new Material(Shader.Find("UI/RendererShader601"));
+                _material = rd.material;
+            }
 
-        //    _material.SetFloat("_yStrideScale", _textureManagerYUV.YStrideScale);
-        //}
+            _material.SetFloat("_yStrideScale", _textureManagerYUV.YStrideScale);
+        }
 
     }
 }
