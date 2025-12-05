feb952325e03b3650ee74900bad1f4507c1f3deb
WinterPu <WinterPu@users.noreply.github.com>
2025-12-05 07:12:07 +0000

    [AUTO] Generate codes by terra(rtc_4.5.2.6)

--- Agora-C_Sharp-RTC-SDK/Code/Rtc/IRtcEngineEx.cs
+++ Agora-C_Sharp-RTC-SDK/Code/Rtc/IRtcEngineEx.cs
@@ -1052,5 +1052,20 @@ namespace Agora.Rtc
         ///
         public abstract int SendAudioMetadataEx(RtcConnection connection, byte[] metadata, ulong length);
 
+        ///
+        /// @ignore
+        ///
+        public abstract int EnableVideoImageSourceEx(bool enable, ImageTrackOptions options, RtcConnection connection);
+
+        ///
+        /// @ignore
+        ///
+        public abstract int PreloadEffectEx(RtcConnection connection, int soundId, string filePath, int startPos = 0);
+
+        ///
+        /// @ignore
+        ///
+        public abstract int PlayEffectEx(RtcConnection connection, int soundId, string filePath, int loopCount, double pitch, double pan, int gain, bool publish = false, int startPos = 0);
+
     }
 }
